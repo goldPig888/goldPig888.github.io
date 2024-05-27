c5d4585f2e03dfb846d8da82f3dbe508fedec670
May Lynn Espinola <maylynnespinola@gmail.com>
2024-05-27 13:48:26 -0500

    fixed link active issues, phone size issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,87 +1,70 @@
 'use strict';
 
-
-
-// element toggle function
+// Element toggle function
 const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
 
-
-
-// sidebar variables
+// Sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// sidebar toggle functionality for mobile
+// Sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
 
-
-
-// testimonials variables
+// Testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
 const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
-// modal variable
+// Modal variable
 const modalImg = document.querySelector("[data-modal-img]");
 const modalTitle = document.querySelector("[data-modal-title]");
 const modalText = document.querySelector("[data-modal-text]");
 
-// modal toggle function
+// Modal toggle function
 const testimonialsModalFunc = function () {
   modalContainer.classList.toggle("active");
   overlay.classList.toggle("active");
 }
 
-// add click event to all modal items
+// Add click event to all modal items
 for (let i = 0; i < testimonialsItem.length; i++) {
-
   testimonialsItem[i].addEventListener("click", function () {
-
     modalImg.src = this.querySelector("[data-testimonials-avatar]").src;
     modalImg.alt = this.querySelector("[data-testimonials-avatar]").alt;
     modalTitle.innerHTML = this.querySelector("[data-testimonials-title]").innerHTML;
     modalText.innerHTML = this.querySelector("[data-testimonials-text]").innerHTML;
-
     testimonialsModalFunc();
-
   });
-
 }
 
-// add click event to modal close button
+// Add click event to modal close button
 modalCloseBtn.addEventListener("click", testimonialsModalFunc);
 overlay.addEventListener("click", testimonialsModalFunc);
 
-
-
-// custom select variables
+// Custom select variables
 const select = document.querySelector("[data-select]");
 const selectItems = document.querySelectorAll("[data-select-item]");
-const selectValue = document.querySelector("[data-selecct-value]");
+const selectValue = document.querySelector("[data-select-value]");
 const filterBtn = document.querySelectorAll("[data-filter-btn]");
 
 select.addEventListener("click", function () { elementToggleFunc(this); });
 
-// add event in all select items
+// Add event in all select items
 for (let i = 0; i < selectItems.length; i++) {
   selectItems[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     elementToggleFunc(select);
     filterFunc(selectedValue);
-
   });
 }
 
-// filter variables
+// Filter variables
 const filterItems = document.querySelectorAll("[data-filter-item]");
 
 const filterFunc = function (selectedValue) {
-
   for (let i = 0; i < filterItems.length; i++) {
-
     if (selectedValue === "all") {
       filterItems[i].classList.add("active");
     } else if (selectedValue === filterItems[i].dataset.category) {
@@ -89,72 +72,63 @@ const filterFunc = function (selectedValue) {
     } else {
       filterItems[i].classList.remove("active");
     }
-
   }
-
 }
 
-// add event in all filter button items for large screen
+// Add event in all filter button items for large screen
 let lastClickedBtn = filterBtn[0];
 
 for (let i = 0; i < filterBtn.length; i++) {
-
   filterBtn[i].addEventListener("click", function () {
-
     let selectedValue = this.innerText.toLowerCase();
     selectValue.innerText = this.innerText;
     filterFunc(selectedValue);
-
     lastClickedBtn.classList.remove("active");
     this.classList.add("active");
     lastClickedBtn = this;
-
   });
-
 }
 
-
-
-// contact form variables
+// Contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
+// Add event to all form input fields
 for (let i = 0; i < formInputs.length; i++) {
   formInputs[i].addEventListener("input", function () {
-
-    // check form validation
+    // Check form validation
     if (form.checkValidity()) {
       formBtn.removeAttribute("disabled");
     } else {
       formBtn.setAttribute("disabled", "");
     }
-
   });
 }
 
-
-
-// page navigation variables
+// Page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
+// Add event to all nav links
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
+    const page = this.innerHTML.toLowerCase();
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
-        window.scrollTo(0, 0);
+    // Add active class to the clicked link and corresponding page
+    for (let j = 0; j < pages.length; j++) {
+      if (page === pages[j].dataset.page) {
+        pages[j].classList.add("active");
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[j].classList.remove("active");
       }
     }
 
+    // Remove active class from all links and add to the clicked one
+    navigationLinks.forEach(link => link.classList.remove("active"));
+    this.classList.add("active");
+
+    window.scrollTo(0, 0);
   });
 }
 
@@ -172,7 +146,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
         const updateCount = () => {
           const currentCount = +counter.innerText;
-
           if (currentCount < target) {
             counter.innerText = Math.ceil(currentCount + increment);
             setTimeout(updateCount, 600); // Adjust timeout to control speed
@@ -198,32 +171,38 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
-//this is for the background color change
+// Background color change
 let ticking = false;
 
-  function updateSmokyBlack() {
-    const scrollTop = window.scrollY;
-    const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-    const scrollFraction = scrollTop / docHeight;
+function updateSmokyBlack() {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollFraction = scrollTop / docHeight;
 
-    const startColor = [244, 89, 7]; 
-    const endColor = [228, 47, 26];
+  const startColor = [244, 89, 7];
+  const endColor = [228, 47, 26];
 
-    const newColor = startColor.map((start, index) => {
-      const end = endColor[index];
-      return start + (end - start) * scrollFraction;
-    });
+  const newColor = startColor.map((start, index) => {
+    const end = endColor[index];
+    return start + (end - start) * scrollFraction;
+  });
 
-    const newHSL = `hsl(${newColor[0]}, ${newColor[1]}%, ${newColor[2]}%)`;
-    document.documentElement.style.setProperty('--smoky-black', newHSL);
+  const newHSL = `hsl(${newColor[0]}, ${newColor[1]}%, ${newColor[2]}%)`;
+  document.documentElement.style.setProperty('--smoky-black', newHSL);
 
-    ticking = false;
+  ticking = false;
+}
+
+document.addEventListener('scroll', () => {
+  if (!ticking) {
+    window.requestAnimationFrame(updateSmokyBlack);
+    ticking = true;
   }
+});
 
-  document.addEventListener('scroll', () => {
-    if (!ticking) {
-      window.requestAnimationFrame(updateSmokyBlack);
-      ticking = true;
-    }
+  const twitterLink = document.querySelector('.twitter-link');
+  
+  twitterLink.addEventListener('click', function () {
+    const url = twitterLink.getAttribute('data-url');
+    window.open(url, '_blank');
   });
